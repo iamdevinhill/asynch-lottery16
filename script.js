1f435d80df3801a4debbfdaa781de4688bfda7aa
Devin Hill <33045257+iamdevinhill@users.noreply.github.com>
2022-03-26 13:31:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const lotteryPromise = new Promise(function (resolve, reject) {
+
+    setTimeout(function () {
+        if (Math.random() >= 0.5) {
+            resolve('You win')
+        } else {
+            reject(new Error('You lost your money'))
+        }
+    }, 2000)
+})
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err))
